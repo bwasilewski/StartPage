bf985ae80f4e366875a1ada7199801baa07361a1
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-11 01:27:08 -0500

    add dotenv config, remove stupid semicolons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import dotenv from 'dotenv'
+// import './index.css'
+import App from './App.jsx'
+import * as serviceWorker from './serviceWorker'
+import '../node_modules/bulma/bulma.sass'
+
+dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
